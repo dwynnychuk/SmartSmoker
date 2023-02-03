b91a77d77e648557260e2f0ed2cad64ac1b12343
dwynnychuk <dwynnychuk@gmail.com>
2023-02-02 20:07:40 -0700

    Initial Commit

---
+++ SmartSmoker/SmartSmokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSmokerApp.swift
+//  SmartSmoker
+//
+//  Created by Dallyn Wynnychuk on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct SmartSmokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
