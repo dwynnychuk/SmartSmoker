b874d7a5f97205c8c9622edb8d16031bad6fee62
dwynnychuk <dwynnychuk@gmail.com>
2023-02-05 19:26:55 -0700

    Accelerometer working, issue with calibration

--- ESP32Code/src/main.cpp
+++ ESP32Code/src/main.cpp
@@ -3,19 +3,57 @@
 #define LED 2
 
 const int MPU = 0x68;
-int16_t AccX, AccY, AccZ, Tmp;
-int16_t GyroX, GyroY, GyroZ;
-int16_t AccAngleX, AccAngleY, GyroAngleX, GyroAngleY, GyroAngleZ;
-float roll, pitch, yaw;
-int16_t AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+int16_t AccX, AccY, AccZ, Tmp, GyroX, GyroY, GyroZ;
+int16_t AccErrorX, AccErrorY, AccErrorZ, GyroErrorX, GyroErrorY, GyroErrorZ;
+float AccAngleX, AccAngleY, AccAngleZ, GyroAngleX, GyroAngleY, GyroAngleZ, BoardTemp;
 float ElapsedTime, CurrentTime, PreviousTime;
-int c = 0;
-
 
 void calculate_IMU_Error() {
   // specified number of static readings to determine offset
+  int c = 0;
   int num_error = 500;
-  Serial.println(num_error);
+  Serial.begin(115200);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // Begin with register 0x3B Accel_Xout_H
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14);
+  int16_t tn = Wire.read();
+
+  while (c < num_error) {
+    AccX = (tn << 8) | Wire.read();
+    tn = Wire.read();
+    AccY = (tn << 8) | Wire.read();
+    tn = Wire.read();
+    AccZ = (tn << 8) | Wire.read();
+    tn = Wire.read();
+    Tmp = (tn << 8) | Wire.read();
+    tn = Wire.read();
+    GyroX = (tn << 8) | Wire.read();
+    tn = Wire.read();
+    GyroY = (tn << 8) | Wire.read();
+    tn = Wire.read();
+    GyroZ = (tn << 8) | Wire.read();
+    tn = Wire.read();
+
+    // Sum values
+    AccErrorX = AccErrorX + AccX;
+    AccErrorY = AccErrorY + AccY;
+    AccErrorZ = AccErrorZ + AccZ;
+    GyroErrorX = GyroErrorX + GyroX;
+    GyroErrorY = GyroErrorY + GyroY;
+    GyroErrorZ = GyroErrorZ + GyroZ;
+    Serial.println(c);
+    c++;
+  }
+  // Divide
+  AccErrorX = AccErrorX / num_error;
+  AccErrorY = AccErrorY / num_error;
+  AccErrorZ = AccErrorZ / num_error;
+  GyroErrorX = GyroErrorX / num_error;
+  GyroErrorY = GyroErrorY / num_error;
+  GyroErrorZ = GyroErrorZ / num_error;
+
+  Serial.println("FIND THE ERROR");
 }
 
 
@@ -23,14 +61,27 @@ void setup() {
   pinMode (LED, OUTPUT);
   Serial.begin(115200);
   Wire.begin();
+
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
 
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
   calculate_IMU_Error();
+  delay(4000);
+  Serial.print("Hello");
   delay(2000);
-  }
+}
 
 
 void loop() {
@@ -59,16 +110,29 @@ void loop() {
   t = Wire.read();
   GyroZ = (t << 8) | Wire.read();
   t = Wire.read();
+
+  AccAngleX = (AccX - AccErrorX) / 16384.0;
+  AccAngleY = (AccY - AccErrorY) / 16384.0;
+  AccAngleZ = (AccZ - AccErrorZ)/ 16384.0;
+  BoardTemp = (Tmp / 340) + 36.53;
+  GyroAngleX = GyroX / 131.0;
+  GyroAngleY = GyroY / 131.0;
+  GyroAngleZ = GyroZ / 131.0;
   
   // Printing to Serial Monitor
-  Serial.print(Tmp);
+  Serial.print(BoardTemp);
   Serial.print(" / ");
-  Serial.print(AccX);
+  Serial.print(AccAngleX);
   Serial.print(" / ");
-  Serial.print(AccY);
+  Serial.print(AccAngleY);
   Serial.print(" / ");
-  Serial.println(AccZ);
-  
+  Serial.print(AccAngleZ);
+  Serial.print(" / ");
+  Serial.print(GyroX);
+  Serial.print(" / ");
+  Serial.print(GyroY);
+  Serial.print(" / ");
+  Serial.println(GyroZ);
   
 
   digitalWrite(LED, LOW);
