04cdf718486384183ec9ea8251e3bfa3ddf83004
dwynnychuk <dwynnychuk@gmail.com>
2023-07-22 15:36:55 -0600

    code cleanup and begin to read output of thermocouples

--- ESP32Code/src/main.cpp
+++ ESP32Code/src/main.cpp
@@ -7,14 +7,11 @@
 #include <BLE2902.h>
 
 #define LED 17 // Internal LED
-
 #define MPA0 2 // A0 for Multiplexer
 #define MPA1 15 // A1 for Multiplexer
-
 #define cs 5 // SPI CS
 #define clk 18 // SPI SCK
 #define miso 19 // SPI MISO
-
 
 const int MPU = 0x68; // IMU
 int16_t AccX, AccY, AccZ, Tmp, GyroX, GyroY, GyroZ;
@@ -24,20 +21,15 @@ float ElapsedTime, CurrentTime, PreviousTime;
 int v = 0;
 float ambientC, internalC, foodC1, foodC2; // measured temperatures in celcius
 
-
 // SPI reading for temperature
 int spiRead() {
   int rawtmp = 0;
+  int NC = 0;
   digitalWrite(cs, LOW);
-  delay(2);
-  digitalWrite(cs, HIGH);
   delay(200);
-
-  // bit 15
-  digitalWrite(cs,LOW); // Start conversation
   digitalWrite(clk, HIGH);
-  delay(1);
-  digitalWrite(clk,LOW); // bit read on down
+  NC += digitalRead(miso);
+  digitalWrite(clk,LOW); 
 
   // bit 14 - 0
   for (int i = 14; i > 0; i--){
@@ -45,96 +37,20 @@ int spiRead() {
     rawtmp += digitalRead(miso) << i;
     digitalWrite(clk,LOW);
   }
+  digitalWrite(cs, HIGH);
 
-  if ((rawtmp & 0x04) == 0x04) return -1;
+  if (NC == 0x00) return 0;
   return rawtmp >> 3;
 }
 
-/*
-// BLE
-#define environmentalService BLEUUID((uint16_t)0x181A)
-BLECharacteristic temperatureCharacteristic(
-  BLEUUID((uint16_t)0x2A6E),
-  BLECharacteristic::PROPERTY_READ  |
-  BLECharacteristic::PROPERTY_NOTIFY
-);
-
-int txValue = 0;
-bool deviceConnected = false;
-class ServerCallbacks: public BLEServerCallbacks{
-  void onConnect(BLEServer* pServer){
-    deviceConnected = true;
-  }
-
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-  }
-};
-
 void readTemperature();
-*/
 
 // Setup
 void setup() {
   pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  Wire.begin();
 }
-
-//  Serial.begin(115200);
-//  Wire.begin();
-
-  // ambient temperature
-  /*
-  pinMode(cs, OUTPUT);
-  pinMode(clk, OUTPUT);
-  pinMode(miso,INPUT);
-  digitalWrite(cs, HIGH);
-  digitalWrite(clk, LOW);
-  */
-
-  // IMU
-  /*
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1C);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  */
-
-/*
-  // BLE
-  BLEDevice::init("ESP32"); // initialize device
-
-  // Create BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ServerCallbacks());
-
-  // Create BLE Service
-  BLEService *pEnvironment = pServer->createService(environmentalService);
-
-  // Create BLE Characteristic
-  pEnvironment->addCharacteristic(&temperatureCharacteristic);
-
-  // Create BLE Descriptor
-  temperatureCharacteristic.addDescriptor(new BLE2902());
-  BLEDescriptor temperatureDescriptor(BLEUUID((uint16_t)0x2901));
-  temperatureDescriptor.setValue("Temperature -40 - 40C");
-  temperatureCharacteristic.addDescriptor(&temperatureDescriptor);
-
-  // Start the service
-  pServer->getAdvertising()->addServiceUUID(environmentalService);
-  pEnvironment->start();
-  Serial.println("Waiting for a client connection...");
-}
-*/
 
 // Loop indefinitely 
 void loop() {
@@ -147,7 +63,8 @@ void loop() {
   digitalWrite(MPA0, 0);
   digitalWrite(MPA1, 0);
   digitalWrite(LED, HIGH);
-  delay(1000);
+  readTemperature();
+  delay(2000);
   digitalWrite(LED, LOW);
 
   // Temperature 2
@@ -162,7 +79,7 @@ void loop() {
   digitalWrite(MPA0, 1);
   digitalWrite(MPA1, 0);
   digitalWrite(LED, HIGH);
-  delay(1000);
+  delay(2000);
   digitalWrite(LED, LOW);
 
   // Temperature 3
@@ -181,7 +98,7 @@ void loop() {
   digitalWrite(MPA0, 0);
   digitalWrite(MPA1, 1);
   digitalWrite(LED, HIGH);
-  delay(1000);
+  delay(2000);
   digitalWrite(LED, LOW);
 
   // Temperature 4
@@ -204,10 +121,26 @@ void loop() {
   digitalWrite(MPA0, 1);
   digitalWrite(MPA1, 1);
   digitalWrite(LED, HIGH);
-  delay(1000);
+  delay(2000);
   digitalWrite(LED, LOW);
 
-  // IMU
+  
+}
+
+void readTemperature(){
+  v = spiRead();
+  if (v == 0) {
+    Serial.print("Temperature sensor not found\n");
+  }
+  else {
+    ambientC = v * 0.25;
+    Serial.println(ambientC);
+  }
+}
+
+
+
+// IMU
   /*
   Wire.beginTransmission(MPU);
   Wire.write(0x3B); // Begin with register 0x3B Accel_Xout_H
@@ -252,47 +185,67 @@ void loop() {
   Serial.print(GyroY);
   Serial.print(" / ");
   Serial.println(GyroZ);
-  */
-
+  */  
   /*
-  readTemperature();
-  Serial.println(ambientC);
-  // BLE
-  if (deviceConnected){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
 
-    delay(500);
-  }
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);
+  Wire.write(0);
+  Wire.endTransmission(true);
   */
 
-  // Photoresistor
+// BLE
   /*
-  prValue = analogRead(PRMEASURE);
-  if (prValue > 100) {
-    digitalWrite(LED,HIGH);
-    Serial.print(prValue);
-    Serial.println(" greater");
-  }
-  else{
-    digitalWrite(LED,LOW);
-    Serial.print(prValue);
-    Serial.println(" Lessthan");
-  }
-  delay(250);
-  //digitalWrite(LED, LOW);
-  delay(250);
-  */
-  
-}
+  BLEDevice::init("ESP32"); // initialize device
 
+  // Create BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
 
-/*
-void readTemperature(){
-  v = spiRead();
-  if (v == -1) {
-    Serial.print("Temperature sensor not found");
-  }
-  else {
-    ambientC = v * 0.25;
-  }
+  // Create BLE Service
+  BLEService *pEnvironment = pServer->createService(environmentalService);
+
+  // Create BLE Characteristic
+  pEnvironment->addCharacteristic(&temperatureCharacteristic);
+
+  // Create BLE Descriptor
+  temperatureCharacteristic.addDescriptor(new BLE2902());
+  BLEDescriptor temperatureDescriptor(BLEUUID((uint16_t)0x2901));
+  temperatureDescriptor.setValue("Temperature -40 - 40C");
+  temperatureCharacteristic.addDescriptor(&temperatureDescriptor);
+
+  // Start the service
+  pServer->getAdvertising()->addServiceUUID(environmentalService);
+  pEnvironment->start();
+  Serial.println("Waiting for a client connection...");
 }
+*/
+  /*
+#define environmentalService BLEUUID((uint16_t)0x181A)
+BLECharacteristic temperatureCharacteristic(
+  BLEUUID((uint16_t)0x2A6E),
+  BLECharacteristic::PROPERTY_READ  |
+  BLECharacteristic::PROPERTY_NOTIFY
+);
+
+int txValue = 0;
+bool deviceConnected = false;
+class ServerCallbacks: public BLEServerCallbacks{
+  void onConnect(BLEServer* pServer){
+    deviceConnected = true;
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
 */
