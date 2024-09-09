00a3e8256efaacf10deb338b95337b2dcdbda551
dwynnychuk <dwynnychuk@gmail.com>
2024-09-08 18:29:05 -0600

    GUI filed generated

---
+++ GUI/code/gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter as tk
+from gui_functions import *
+import time
+import serial
+
+## Function definitions
+turn_led_on()
+turn_led_off()
+root = tk.Tk()
---
+++ GUI/code/gui_functions.py
@@ -0,0 +1,18 @@
+# Functions for gui.py
+
+# LED Functions
+def turn_led_on():
+    print("on")
+
+def turn_led_off():
+    print("off")
+
+def blink_led():
+    pass
+
+# Measurement Functions
+
+
+# Logging Functions
+
+
