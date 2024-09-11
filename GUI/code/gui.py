d78d0832b36b456e779e32e0f5391cd939abf19b
dwynnychuk <dwynnychuk@gmail.com>
2024-09-10 20:39:54 -0600

    cleanup

--- GUI/code/gui.py
+++ GUI/code/gui.py
@@ -1,10 +1,19 @@
 from tkinter import *
-import tkinter as tk
 from gui_functions import *
-import time
 import serial
+import time
 
-## Function definitions
-turn_led_on()
-turn_led_off()
-root = tk.Tk()
+# Initialize Tkinter window
+root = Tk()
+root.title("GUI Test")
+root.geometry("300x200")
+
+# Tkinter Widgets
+#button_on = Button(root, text="Turn LED On", command=turn_led_on)
+#button_on.pack(pady=10)
+
+#button_off = Button(root, text="Turn LED Off", command=turn_led_off)
+#button_off.pack(pady=10)
+
+# Run the Tkinter event loop
+root.mainloop()
