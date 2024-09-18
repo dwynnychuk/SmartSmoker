61f31f9ded4333ed153d9b48540535a1f1a3b753
dwynnychuk <dwynnychuk@gmail.com>
2024-09-17 21:03:56 -0600

    Logging buttons and functionality working

--- GUI/code/gui.py
+++ GUI/code/gui.py
@@ -1,13 +1,17 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 import tkinter.ttk as ttk
+from tkinter.filedialog import asksaveasfilename
 from gui_functions import *
 import serial
 import time
+import datetime
 
 # Variable Initializations
 paddingX = 10
 paddingY = 10
+loggingFilepath = None
+logFile = None
 
 # Initialize Tkinter window
 root = Tk()
@@ -16,7 +20,48 @@ root.geometry("1000x800")
 
 # Examples, need to connect
 connectedPort = "COM4"
-loggingFilepath = "C:/Desktop"
+
+# Functions
+def logging_start():
+    global loggingFilepath, logFile
+    tempLogFile = "log_" + datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+    loggingFilepath = asksaveasfilename(defaultextension=".csv",
+                                        initialdir="/Users/dallynwynnychuk/Documents/Projects/Smart Smoker/SmartSmoker/Data",
+                                        initialfile=tempLogFile,
+                                        filetypes=[("CSV files", "*.csv"),("All Files", "*.*")]) # Label pattern tuples
+    if loggingFilepath:
+        label_loggingto.config(text=f"Logging Location: {loggingFilepath}")
+        button_startlog.config(state="disabled")
+        
+        logFile = open(loggingFilepath,'w')
+        logFile.write("Time, Temperature\n")
+        
+        # Schedule data read each second
+        readData()
+        
+
+def logging_stop():
+    global logFile
+    if logFile:
+        logFile.close()
+        logFile = None
+        button_startlog.config(state="normal")
+        window_output.insert(END, "Logging stopped\n")
+        window_output.see(END)
+
+def readData():
+    global logFile
+    if logFile:
+        print("reading\n")
+        timeStamp = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+        tempData = "100"
+        logFile.write(f"{timeStamp}, {tempData}\n")
+        logFile.flush()
+        
+        window_output.insert(END, f"{timeStamp}, {tempData}\n")
+        window_output.see(END)
+        
+    root.after(1000, readData)
 
 # Tkinter Widgets
 # Connection Frame
@@ -54,7 +99,6 @@ label_loggingto.grid(row=1, column=0, padx=paddingX, pady=paddingY)
 window_output = ScrolledText(root, wrap=WORD, width=100, height=25)
 window_output.grid(row=3, column=0, columnspan=4, padx=paddingX, pady=paddingY)
         
-print_hello_world(window_output, END)
 
 # Run the Tkinter event loop
 root.mainloop()
--- GUI/code/gui_functions.py
+++ GUI/code/gui_functions.py
@@ -17,22 +17,7 @@ def connect_to_grill():
 def disconnect_from_grill():
     pass
 
-# Measurement Functions
-
-
-# Logging Functions
-def logging_start():
-    pass
-
-def logging_stop():
-    pass
-
 # Serial Functions
 def read_serial():
     pass
 
-def print_hello_world(window, end_marker, i=0):
-    if i < 100:
-        window.insert(end_marker, f"Hello world {i}\n")
-        window.see(end_marker)
-        window.after(100, print_hello_world, window, end_marker, i+1)
