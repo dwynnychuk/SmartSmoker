30f6781ad273bf00d7cdd8b8e888fa09a593df4c
dwynnychuk <dwynnychuk@gmail.com>
2026-04-08 20:49:09 -0700

    RTD class tidied up

--- ESP32Code/include/RTD.h
+++ ESP32Code/include/RTD.h
@@ -6,11 +6,11 @@
 
 class RTD : public ISensor {
 private:
-    int pinPlus;
-    int pinMinus;
+    int pin_plus;
+    int pin_minus;
 
-    float vref;
-    float rref;
+    float v_ref;
+    float r_ref;
 
     float temperature;
     float voltage;
@@ -20,7 +20,7 @@ private:
     float ResistanceToTemperature(float r);
 
 public: 
-    RTD(int analogPinPlus, int analogPinMinus, float vref, float rref);
+    RTD(int analog_pin_plus, int analog_pin_minus, float v_ref, float r_ref);
 
     void begin() override;
     void update() override;
--- ESP32Code/src/sensors/RTD.cpp
+++ ESP32Code/src/sensors/RTD.cpp
@@ -1,29 +1,37 @@
 #include "RTD.h"
 
-RTD::RTD(int analogPinPlus, int analogPinMinus, float vref, float rref)
-    : pinPlus(analogPinPlus), pinMinus(analogPinMinus), vref(vref), rref(rref),
+RTD::RTD(int analog_pin_plus, int analog_pin_minus, float v_ref, float r_ref)
+    : pin_plus(analog_pin_plus), pin_minus(analog_pin_minus), v_ref(v_ref), r_ref(r_ref),
     temperature(0), voltage(0) {}
 
 void RTD::begin() {
-    pinMode(pinPlus, INPUT);
-    pinMode(pinMinus, INPUT);
+    pinMode(pin_plus, INPUT);
+    pinMode(pin_minus, INPUT);
 }
 
 float RTD::readVoltage(int pin) {
     int raw = analogRead(pin);
-    return (raw / 4095.0) * vref; // 12bit
+    return (raw / 4095.0) * v_ref; // 12bit
 }
 
 float RTD::voltageToResistance(float v_out) {
-    float x = 0.5 - (v_out / vref);
+    float x = 0.5 - (v_out / v_ref);
 
     // precent dividing by zero
     if (x < 0.01f) x = 0.01f;
     if (x > 0.99f) x = 0.99f;
 
-    return rref * (x / (1.0f - x));
+    return r_ref * (x / (1.0f - x));
 }
 
 void RTD::update() {
-    //
+    float v_plus = readVoltage(pin_plus);
+    float v_minus = readVoltage(pin_minus);
+
+    float v_out = v_plus - v_minus;
+    voltage = v_out;
+
+    float resistance = voltageToResistance(v_out);
+
+    temperature = (resistance - 100.0f) / 0.385f; //pt 100 estimation
 }
