4026a8f40104f6afd7c3d882246e743172aaeafb
dwynnychuk <dwynnychuk@gmail.com>
2026-05-30 21:42:24 -0700

    Telemetry struct development

--- ESP32Code/include/Telemetry.h
+++ ESP32Code/include/Telemetry.h
@@ -1,10 +1,29 @@
 #ifndef TELEMETRY_H
 #define TELEMETRY_H
+#include <stdint.h>
 
-class Telemetry {
-public:
-    Telemetry();
-private:
+struct Telemetry {
+    // Temperatures
+    float tempRTD = 0.0f;
+    float tempProbe = 0.0f;
+
+    // Ambient Light Sensor
+    uint16_t ambientRaw = 0;
+
+    // IMU
+    int32_t accel[3] = {};
+    int32_t gyro[3] = {};
+
+    // Load States
+    float augerDuty = 0.0f;
+    float ignitorDuty = 0.0f;
+    float fanDuty = 0.0f;
+
+    // Time
+    uint32_t timestamp = 0;
+
+    // Flags
+    bool lidOpen = false;
 
 };
 
